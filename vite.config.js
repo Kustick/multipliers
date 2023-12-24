a9caeee5501602a05fb8c921b3c5fc3c3bd0e139
Kustick <66900070+Kustick@users.noreply.github.com>
2023-12-24 22:08:21 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import Inspect from 'vite-plugin-inspect'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/multipliers/',
   plugins: [
     react(),
-    Inspect()
   ],
-})
+});
